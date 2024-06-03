24663ab3fa02889d967b03ca191647dcf0de69ad
Jesus <jlarab@esliceu.net>
2024-06-03 09:45:06 +0200

    web en php

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+//parametres de connexió
+$servername="localhost";
+$username="jesus";
+$password="12344";
+$dbname="gimnasio";
+
+//crear connexió
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+//comprovar connexió
+if($conn->connect_error){
+    //la conexion ha sido mala
+    die("Connection failed: ". $conn->connect_error);
+}
+
+echo "LA CONEXION HA SIDO UN EXITO";
--- index.php
+++ index.php
@@ -1,48 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenidos a la web de Jesús</title>
-    <link rel="stylesheet" href="normalize.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-    <header>
-        <h1 class="titulo">Jesús Lara <span>Web</span></h1>
-    </header>
-    <div class="bg-secundario">
-        <nav class="navegacion-principal">
-                <a href="index.html">Home</a>
-                <a href="nosotros.html">Nosotros</a>
-                <a href="contacto.html">Contacto</a>
-                <a href="rutinas.html">Rutinas</a>
-                <a href="entrenamiento.html">Entrenamiento</a>
-        </nav>
-    </div>
-    <section class="hero">
-        <div class="contenido-hero">
-            <h2>Rutinas i entrenamientos personalizados</h2>
-            <div class="informacion">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-barbell" width="80" height="80" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M2 12h1" />
-                    <path d="M6 8h-2a1 1 0 0 0 -1 1v6a1 1 0 0 0 1 1h2" />
-                    <path d="M6 7v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1 -1v-10a1 1 0 0 0 -1 -1h-1a1 1 0 0 0 -1 1z" />
-                    <path d="M9 12h6" />
-                    <path d="M15 7v10a1 1 0 0 0 1 1h1a1 1 0 0 0 1 -1v-10a1 1 0 0 0 -1 -1h-1a1 1 0 0 0 -1 1z" />
-                    <path d="M18 8h2a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-2" />
-                    <path d="M22 12h-1" />
-                  </svg>
-                <p>Consigue el físico de tus sueños</p>
-            </div>
-            <a class="boton" href="entrenamiento.html">Entrenamiento</a>
-        </div>
-    </section>
-    <main class="contenedor sombra">
+<?php 
+    include 'header.php';
+?>
+<main class="contenedor sombra">
         <h2>Mi contenido</h2>
         <div class="micontenido">
             <section class="contenido">
@@ -112,8 +71,6 @@
             
         </section>
     </main>
-    <footer>
-        <p>Derechos reservados</p>
-    </footer>
-</body>
-</html>
+<?php
+    include 'footer.php';
+?>
